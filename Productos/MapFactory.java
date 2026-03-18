e03bbef52065b5910b5061c57eef2610b3a560c2
Dimaroca <dimateoroca@gmail.com>
2026-03-18 11:15:58 -0600

    Project orderes, README and .gitignore added

--- Productos/MapFactory.java
+++ Productos/MapFactory.java
@@ -1,7 +1,8 @@
+package Productos;
 import java.util.HashMap;
-import java.util.TreeMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapFactory {
 
