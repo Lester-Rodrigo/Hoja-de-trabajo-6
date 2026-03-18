8ac8aab564267e131a8bf1d63bf5a5a5e086f903
Dimaroca <dimateoroca@gmail.com>
2026-03-18 11:39:50 -0600

    Class Product added

---
+++ Productos/Product.java
@@ -0,0 +1,21 @@
+public class Product{
+    String name;
+    int amount;
+
+    public Product(String name){
+        this.name = name;
+        this.amount=0;
+    }
+
+    public void setAmount(){
+        amount++;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+}
